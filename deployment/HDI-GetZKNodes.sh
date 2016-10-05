0d89e6caa812d26389f16c30df64f3c62d39752f
James Baker <jamesbak@microsoft.com>
2016-10-05 12:43:34 -0700

    Updated deployment script + metrics & theme fixup

--- deployment/HDI-GetZKNodes.sh
+++ deployment/HDI-GetZKNodes.sh
@@ -1,29 +1,71 @@
 #!/bin/bash          
 
-opentsdb_ver=2.2.0
 user=admin
 password=P0rsche911!
 
-wget https://raw.githubusercontent.com/OpenTSDB/opentsdb/v${opentsdb_ver}/src/create_table.sh
-chmod +x create_table.sh
-HBASE_HOME=/usr COMPRESSION=SNAPPY ./create_table.sh
+apt-get install jq
+wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O /usr/bin/jq
 
-wget https://github.com/OpenTSDB/opentsdb/releases/download/v${opentsdb_ver}/opentsdb-${opentsdb_ver}_all.deb
-sudo dpkg -i opentsdb-${opentsdb_ver}_all.deb
-
-#sudo apt-get install jq
-wget http://stedolan.github.io/jq/download/linux64/jq 
-chmod +x ./jq
-
-cluster=$(curl -u $user:$password http://headnodehost:8080/api/v1/clusters | ./jq -r .items[0].Clusters.cluster_name)
-latest_tag=$(curl -u $user:$password -k "http://headnodehost:8080/api/v1/clusters/$cluster/configurations?type=hbase-site" | ./jq -r '.items | sort_by(.version) | reverse | .[0].tag')
-zk_quorum=$(curl -u $user:$password -k "http://headnodehost:8080/api/v1/clusters/$cluster/configurations?type=hbase-site&tag=$latest_tag" | ./jq -r '.items[0].properties."hbase.zookeeper.quorum"')
+cluster=$(curl -u $user:$password http://headnodehost:8080/api/v1/clusters | jq -r .items[0].Clusters.cluster_name)
 stack_name=$(curl -u $user:$password http://headnodehost:8080/api/v1/clusters/$cluster/stack_versions | jq -r '.items[0].ClusterStackVersions.stack')
 stack_version=$(curl -u $user:$password http://headnodehost:8080/api/v1/clusters/$cluster/stack_versions | jq -r '.items[0].ClusterStackVersions.version')
 
-config=/etc/opentsdb/opentsdb.conf
-sudo sed -i '/^#tsd.storage.hbase.zk_quorum* /s/^#//' $config
-sudo sed -i "s/\(tsd.storage.hbase.zk_quorum *= *\).*/\1$zk_quorum/" $config
-sudo sed -i '/^#tsd.storage.hbase.zk_basedir* /s/^#//' $config
-sudo sed -i "s/\(tsd.storage.hbase.zk_basedir *= *\).*/\1\/hbase-unsecure/" $config
+cd /var/lib/ambari-server/resources/stacks/$stack_name/$stack_version/services
+tar -xvf /tmp/OPENTSDB.tar.gz
+chmod -R 644 OPENTSDB
+sed -i "s/\(agent.auto.cache.update=\).*/\1true/" /etc/ambari-server/conf/ambari.properties
+ambari-server refresh-stack-hash
+service ambari-server restart
+sleep 60s
+
+# metrics - add our metrics to the whitelist & recycle the metrics collector
+cat OPENTSDB/metrics.json | jq -r '.OPENTSDB_TSD.Component[0].metrics.default[].metric' >> /etc/ambari-metrics-collector/conf/whitelistedmetrics.txt
+cat OPENTSDB/metrics.json | jq -r '.OPENTSDB_PROXY.Component[0].metrics.default[].metric' >> /etc/ambari-metrics-collector/conf/whitelistedmetrics.txt
+
+su - ams -c'/usr/sbin/ambari-metrics-collector --config /etc/ambari-metrics-collector/conf/ restart'
+
+curl -u $user:$password -H "X-Requested-By:ambari" -X POST -d '{"ServiceInfo":{"service_name":"OPENTSDB"}}' "http://headnodehost:8080/api/v1/clusters/$cluster/services"
+curl -u $user:$password -H "X-Requested-By:ambari" -X POST "http://headnodehost:8080/api/v1/clusters/$cluster/services/OPENTSDB/components/OPENTSDB_TSD"
+curl -u $user:$password -H "X-Requested-By:ambari" -X POST "http://headnodehost:8080/api/v1/clusters/$cluster/services/OPENTSDB/components/OPENTSDB_PROXY"
+config_tag=INITIAL
+curl -u $user:$password -H "X-Requested-By:ambari" -X POST -d '{"type": "opentsdb-site", "tag": "'$config_tag'", "properties" : {
+        "tsd.core.auto_create_metrics" : "true",
+        "tsd.http.cachedir" : "/tmp/opentsdb",
+        "tsd.http.staticroot" : "/usr/share/opentsdb/static/",
+        "tsd.network.async_io" : "true",
+        "tsd.network.keep_alive" : "true",
+        "tsd.network.port" : "4242",
+        "tsd.network.reuse_address" : "true",
+        "tsd.network.tcp_no_delay" : "true",
+        "tsd.storage.enable_compaction" : "true",
+        "tsd.storage.flush_interval" : "1000",
+        "tsd.storage.hbase.data_table" : "tsdb",
+        "tsd.storage.hbase.uid_table" : "tsdb-uid"
+      }}' "http://headnodehost:8080/api/v1/clusters/$cluster/configurations"
+curl -u $user:$password -H "X-Requested-By:ambari" -X POST -d '{"type": "opentsdb-config", "tag": "'$config_tag'", "properties" : {
+        "opentsdb.create_schema" : "true",
+        "opentsdb.opentsdb_version" : "2.2.0"
+      }}' "http://headnodehost:8080/api/v1/clusters/$cluster/configurations"
+curl -u $user:$password -H "X-Requested-By:ambari" -X PUT -d '{"Clusters":{"desired_config" : {"type": "opentsdb-site", "tag": "'$config_tag'"}}}' "http://headnodehost:8080/api/v1/clusters/$cluster"
+curl -u $user:$password -H "X-Requested-By:ambari" -X PUT -d '{"Clusters":{"desired_config" : {"type": "opentsdb-config", "tag": "'$config_tag'"}}}' "http://headnodehost:8080/api/v1/clusters/$cluster"
+
+# deploy to all Region Server nodes
+region_hosts=$(curl -u $user:$password -H "X-Requested-By:ambari" "http://headnodehost:8080/api/v1/clusters/$cluster/services/HBASE/components/HBASE_REGIONSERVER?fields=host_components" | jq -r '.host_components[].HostRoles.host_name')
+for host in $region_hosts; do
+    curl -u $user:$password -H "X-Requested-By:ambari" -X POST "http://headnodehost:8080/api/v1/clusters/$cluster/hosts/$host/host_components/OPENTSDB_TSD"
+done
+
+# install now & wait for the installation to complete
+install_id=$(curl -u $user:$password -H "X-Requested-By:ambari" -X PUT -d '{"RequestInfo": {"context":"Install OPENTSDB daemon services"}, "Body": {"ServiceInfo": {"state": "INSTALLED"}}}' "http://headnodehost:8080/api/v1/clusters/$cluster/services/OPENTSDB" | jq -r '.Requests.id')
+echo "Install request id is: $install_id"
+percent_complete=0
+while [ $percent_complete -lt 100 ]; do
+    percent_complete=$(curl -u $user:$password -H "X-Requested-By:ambari" "http://headnodehost:8080/api/v1/clusters/$cluster/requests/$install_id" | jq -r '.Requests.progress_percent')
+    echo $percent_complete
+    sleep 3s
+done
+
+# finally, start the service
+curl -u $user:$password -H "X-Requested-By:ambari" -X PUT -d '{"RequestInfo": {"context":"Start OPENTSDB services"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' "http://headnodehost:8080/api/v1/clusters/$cluster/services/OPENTSDB"
+
 
