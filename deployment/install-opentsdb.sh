72bdab216e683d56bc73145d36e10e6c342ecaf8
James Baker <jamesbak@microsoft.com>
2016-10-06 18:36:04 -0700

    Added timestamps to logging

--- deployment/install-opentsdb.sh
+++ deployment/install-opentsdb.sh
@@ -3,18 +3,21 @@
 user=$1
 password=$2
 
+echo "$(date +'%Y-%m-%d %H:%M:%S') Starting custom action script for provisioning OpenTSDB as an Ambari service"
 apt-get install jq
 wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O /usr/bin/jq
 
 cluster=$(curl -u $user:$password http://headnodehost:8080/api/v1/clusters | jq -r .items[0].Clusters.cluster_name)
 stack_name=$(curl -u $user:$password http://headnodehost:8080/api/v1/clusters/$cluster/stack_versions | jq -r '.items[0].ClusterStackVersions.stack')
 stack_version=$(curl -u $user:$password http://headnodehost:8080/api/v1/clusters/$cluster/stack_versions | jq -r '.items[0].ClusterStackVersions.version')
+echo "$(date +'%Y-%m-%d %H:%M:%S') Cluster: $cluster, Stack: $stack_name-$stack_version" 
 
 cd /var/lib/ambari-server/resources/stacks/$stack_name/$stack_version/services
 wget "https://github.com/jamesbak/opentsdb-hdi/files/514799/OPENTSDB.tar.gz" -O /tmp/OPENTSDB.tar.gz
 tar -xvf /tmp/OPENTSDB.tar.gz
 chmod -R 644 OPENTSDB
 sed -i "s/\(agent.auto.cache.update=\).*/\1true/" /etc/ambari-server/conf/ambari.properties
+echo "$(date +'%Y-%m-%d %H:%M:%S') Refreshing and restarting Ambari server"
 ambari-server refresh-stack-hash
 service ambari-server restart
 
@@ -26,17 +29,17 @@ cat OPENTSDB/metrics.json | jq -r '.OPENTSDB_PROXY.Component[0].metrics.default[
 head_ip=$(getent hosts headnodehost | awk '{ print $1; exit }')
 if [ "$(hostname -i)" == "$head_ip" ] ; then
 
-    echo "Processing service registration on active head node via background script"
+    echo "$(date +'%Y-%m-%d %H:%M:%S') Processing service registration on active head node via background script"
     wget "https://raw.githubusercontent.com/jamesbak/opentsdb-hdi/v0.3/deployment/create-ambari-services.sh" -O /tmp/create-opentsdb-ambari-services.sh
     chmod 744 /tmp/create-opentsdb-ambari-services.sh
     mkdir /var/log/opentsdb
-    echo "Logging background activity to /var/log/opentsdb/create-ambari-services.out & /var/log/opentsdb/create-ambari-services.err"
+    echo "$(date +'%Y-%m-%d %H:%M:%S') Logging background activity to /var/log/opentsdb/create-ambari-services.out & /var/log/opentsdb/create-ambari-services.err"
     nohup /tmp/create-opentsdb-ambari-services.sh $user $password $cluster >/var/log/opentsdb/create-ambari-services.out 2>/var/log/opentsdb/create-ambari-services.err &
 else
 
-    echo "Node is not currently active head node"
+    echo "$(date +'%Y-%m-%d %H:%M:%S') Node is not currently active head node"
 fi
-echo "OpenTSDB has been installed and TSD components have been deployed to all HBase region servers"
+echo "$(date +'%Y-%m-%d %H:%M:%S') OpenTSDB has been installed and TSD components have been deployed to all HBase region servers"
 
 
 
