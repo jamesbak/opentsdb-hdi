e8f4ee16b83fbba475846d1dbedf3f511aafb997
James Baker <jamesbak@microsoft.com>
2016-10-07 10:23:08 -0700

    Further refactoring of deployment scripts to avoid ambari outage during HDI installation

--- deployment/create-ambari-services.sh
+++ deployment/create-ambari-services.sh
@@ -5,54 +5,73 @@
 user=$1
 password=$2
 cluster=$3
+is_active_headnode=$4
 
-# Ambari has just had a new service installed & has been restarted. We have to wait for it to come back up properly 
-sleep 60s
+# Given that this script is running detached, we need to wait for a bit before restarting Ambari. Rrestarting during the
+# execution of our parent script causes the Ambari task to fail, thus failing the ARM deployment.
+echo "$(date +%T) Pausing to allow remainder of HDInsight provision to complete"
+sleep 180s
 
-echo "Processing service registration on active head node"
-su - ams -c'/usr/sbin/ambari-metrics-collector --config /etc/ambari-metrics-collector/conf/ restart'
+# Restart Ambari to cause our new service artifacts to be registered
+echo "$(date +%T) Restarting Ambari to register OpenTSDB service"
+service ambari-server restart
 
-curl -u $user:$password -H "X-Requested-By:ambari" -X POST -d '{"ServiceInfo":{"service_name":"OPENTSDB"}}' "http://headnodehost:8080/api/v1/clusters/$cluster/services"
-curl -u $user:$password -H "X-Requested-By:ambari" -X POST "http://headnodehost:8080/api/v1/clusters/$cluster/services/OPENTSDB/components/OPENTSDB_TSD"
-curl -u $user:$password -H "X-Requested-By:ambari" -X POST "http://headnodehost:8080/api/v1/clusters/$cluster/services/OPENTSDB/components/OPENTSDB_PROXY"
-config_tag=INITIAL
-curl -u $user:$password -H "X-Requested-By:ambari" -X POST -d '{"type": "opentsdb-site", "tag": "'$config_tag'", "properties" : {
-        "tsd.core.auto_create_metrics" : "true",
-        "tsd.http.cachedir" : "/tmp/opentsdb",
-        "tsd.http.staticroot" : "/usr/share/opentsdb/static/",
-        "tsd.network.async_io" : "true",
-        "tsd.network.keep_alive" : "true",
-        "tsd.network.port" : "4242",
-        "tsd.network.reuse_address" : "true",
-        "tsd.network.tcp_no_delay" : "true",
-        "tsd.storage.enable_compaction" : "true",
-        "tsd.storage.flush_interval" : "1000",
-        "tsd.storage.hbase.data_table" : "tsdb",
-        "tsd.storage.hbase.uid_table" : "tsdb-uid"
-    }}' "http://headnodehost:8080/api/v1/clusters/$cluster/configurations"
-curl -u $user:$password -H "X-Requested-By:ambari" -X POST -d '{"type": "opentsdb-config", "tag": "'$config_tag'", "properties" : {
-        "opentsdb.create_schema" : "true",
-        "opentsdb.opentsdb_version" : "2.2.0"
-    }}' "http://headnodehost:8080/api/v1/clusters/$cluster/configurations"
-curl -u $user:$password -H "X-Requested-By:ambari" -X PUT -d '{"Clusters":{"desired_config" : {"type": "opentsdb-site", "tag": "'$config_tag'"}}}' "http://headnodehost:8080/api/v1/clusters/$cluster"
-curl -u $user:$password -H "X-Requested-By:ambari" -X PUT -d '{"Clusters":{"desired_config" : {"type": "opentsdb-config", "tag": "'$config_tag'"}}}' "http://headnodehost:8080/api/v1/clusters/$cluster"
+# We only need the service registration to proceed once - do it on the active headnode
+if [[ $is_active_headnode ]]; then
+    echo "$(date +%T) Proceeding with registration & installation of OpenTSDB service + components on active head node"
+    # We have to wait for it to come back up properly 
+    sleep 60s
 
-# deploy to all Region Server nodes
-region_hosts=$(curl -u $user:$password -H "X-Requested-By:ambari" "http://headnodehost:8080/api/v1/clusters/$cluster/services/HBASE/components/HBASE_REGIONSERVER?fields=host_components" | jq -r '.host_components[].HostRoles.host_name')
-for host in $region_hosts; do
-    curl -u $user:$password -H "X-Requested-By:ambari" -X POST "http://headnodehost:8080/api/v1/clusters/$cluster/hosts/$host/host_components/OPENTSDB_TSD"
-done
+    echo "$(date +%T) Restarting AMS to make new whitelist metrics effective"
+    su - ams -c'/usr/sbin/ambari-metrics-collector --config /etc/ambari-metrics-collector/conf/ restart'
 
-# install now & wait for the installation to complete
-install_id=$(curl -u $user:$password -H "X-Requested-By:ambari" -X PUT -d '{"RequestInfo": {"context":"Install OPENTSDB daemon services"}, "Body": {"ServiceInfo": {"state": "INSTALLED"}}}' "http://headnodehost:8080/api/v1/clusters/$cluster/services/OPENTSDB" | jq -r '.Requests.id')
-echo "Install request id is: $install_id"
-percent_complete=0
-while [ $percent_complete -lt 100 ]; do
-    complete=$(curl -u $user:$password -H "X-Requested-By:ambari" "http://headnodehost:8080/api/v1/clusters/$cluster/requests/$install_id" | jq -r '.Requests.progress_percent')
-    percent_complete=$(printf "%.0f" $complete)
-    echo $percent_complete
-    sleep 3s
-done
+    echo "$(date +%T) Registering OpenTSDB service with Ambari"
+    curl -u $user:$password -H "X-Requested-By:ambari" -X POST -d '{"ServiceInfo":{"service_name":"OPENTSDB"}}' "http://headnodehost:8080/api/v1/clusters/$cluster/services"
+    curl -u $user:$password -H "X-Requested-By:ambari" -X POST "http://headnodehost:8080/api/v1/clusters/$cluster/services/OPENTSDB/components/OPENTSDB_TSD"
+    curl -u $user:$password -H "X-Requested-By:ambari" -X POST "http://headnodehost:8080/api/v1/clusters/$cluster/services/OPENTSDB/components/OPENTSDB_PROXY"
+    config_tag=INITIAL
+    curl -u $user:$password -H "X-Requested-By:ambari" -X POST -d '{"type": "opentsdb-site", "tag": "'$config_tag'", "properties" : {
+            "tsd.core.auto_create_metrics" : "true",
+            "tsd.http.cachedir" : "/tmp/opentsdb",
+            "tsd.http.staticroot" : "/usr/share/opentsdb/static/",
+            "tsd.network.async_io" : "true",
+            "tsd.network.keep_alive" : "true",
+            "tsd.network.port" : "4242",
+            "tsd.network.reuse_address" : "true",
+            "tsd.network.tcp_no_delay" : "true",
+            "tsd.storage.enable_compaction" : "true",
+            "tsd.storage.flush_interval" : "1000",
+            "tsd.storage.hbase.data_table" : "tsdb",
+            "tsd.storage.hbase.uid_table" : "tsdb-uid"
+        }}' "http://headnodehost:8080/api/v1/clusters/$cluster/configurations"
+    curl -u $user:$password -H "X-Requested-By:ambari" -X POST -d '{"type": "opentsdb-config", "tag": "'$config_tag'", "properties" : {
+            "opentsdb.create_schema" : "true",
+            "opentsdb.opentsdb_version" : "2.2.0"
+        }}' "http://headnodehost:8080/api/v1/clusters/$cluster/configurations"
+    curl -u $user:$password -H "X-Requested-By:ambari" -X PUT -d '{"Clusters":{"desired_config" : {"type": "opentsdb-site", "tag": "'$config_tag'"}}}' "http://headnodehost:8080/api/v1/clusters/$cluster"
+    curl -u $user:$password -H "X-Requested-By:ambari" -X PUT -d '{"Clusters":{"desired_config" : {"type": "opentsdb-config", "tag": "'$config_tag'"}}}' "http://headnodehost:8080/api/v1/clusters/$cluster"
 
-# finally, start the service
-curl -u $user:$password -H "X-Requested-By:ambari" -X PUT -d '{"RequestInfo": {"context":"Start OPENTSDB services"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' "http://headnodehost:8080/api/v1/clusters/$cluster/services/OPENTSDB"
+    # deploy to all Region Server nodes
+    echo "$(date +%T) Installing OpenTSDB TSD component on all HBase region servers"
+    region_hosts=$(curl -u $user:$password -H "X-Requested-By:ambari" "http://headnodehost:8080/api/v1/clusters/$cluster/services/HBASE/components/HBASE_REGIONSERVER?fields=host_components" | jq -r '.host_components[].HostRoles.host_name')
+    for host in $region_hosts; do
+        echo "$(date +%T) Installing OpenTSDB TSD component on host: $host"
+        curl -u $user:$password -H "X-Requested-By:ambari" -X POST "http://headnodehost:8080/api/v1/clusters/$cluster/hosts/$host/host_components/OPENTSDB_TSD"
+    done
+
+    # install now & wait for the installation to complete
+    install_id=$(curl -u $user:$password -H "X-Requested-By:ambari" -X PUT -d '{"RequestInfo": {"context":"Install OPENTSDB daemon services"}, "Body": {"ServiceInfo": {"state": "INSTALLED"}}}' "http://headnodehost:8080/api/v1/clusters/$cluster/services/OPENTSDB" | jq -r '.Requests.id')
+    echo "$(date +%T) Install request id is: $install_id"
+    percent_complete=0
+    while [ $percent_complete -lt 100 ]; do
+        complete=$(curl -u $user:$password -H "X-Requested-By:ambari" "http://headnodehost:8080/api/v1/clusters/$cluster/requests/$install_id" | jq -r '.Requests.progress_percent')
+        percent_complete=$(printf "%.0f" $complete)
+        echo "$(date +%T) Install completion percentage: $percent_complete"
+        sleep 3s
+    done
+
+    # finally, start the service
+    echo "$(date +%T) Starting the OpenTSDB TSD service on all hosts"
+    curl -u $user:$password -H "X-Requested-By:ambari" -X PUT -d '{"RequestInfo": {"context":"Start OPENTSDB services"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' "http://headnodehost:8080/api/v1/clusters/$cluster/services/OPENTSDB"
+fi
+echo "$(date +%T) Completed secondary installation of OpenTSDB service"
--- deployment/install-opentsdb.sh
+++ deployment/install-opentsdb.sh
@@ -3,23 +3,22 @@
 user=$1
 password=$2
 
-echo "$(date +'%Y-%m-%d %H:%M:%S') Starting custom action script for provisioning OpenTSDB as an Ambari service"
+echo "$(date +%T) Starting custom action script for provisioning OpenTSDB as an Ambari service"
 apt-get install jq
 wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O /usr/bin/jq
 
 cluster=$(curl -u $user:$password http://headnodehost:8080/api/v1/clusters | jq -r .items[0].Clusters.cluster_name)
 stack_name=$(curl -u $user:$password http://headnodehost:8080/api/v1/clusters/$cluster/stack_versions | jq -r '.items[0].ClusterStackVersions.stack')
 stack_version=$(curl -u $user:$password http://headnodehost:8080/api/v1/clusters/$cluster/stack_versions | jq -r '.items[0].ClusterStackVersions.version')
-echo "$(date +'%Y-%m-%d %H:%M:%S') Cluster: $cluster, Stack: $stack_name-$stack_version" 
+echo "$(date +%T) Cluster: $cluster, Stack: $stack_name-$stack_version" 
 
 cd /var/lib/ambari-server/resources/stacks/$stack_name/$stack_version/services
 wget "https://github.com/jamesbak/opentsdb-hdi/files/514799/OPENTSDB.tar.gz" -O /tmp/OPENTSDB.tar.gz
 tar -xvf /tmp/OPENTSDB.tar.gz
 chmod -R 644 OPENTSDB
 sed -i "s/\(agent.auto.cache.update=\).*/\1true/" /etc/ambari-server/conf/ambari.properties
-echo "$(date +'%Y-%m-%d %H:%M:%S') Refreshing and restarting Ambari server"
+echo "$(date +%T) Refreshing and restarting Ambari server"
 ambari-server refresh-stack-hash
-service ambari-server restart
 
 # metrics - add our metrics to the whitelist & recycle the metrics collector
 cat OPENTSDB/metrics.json | jq -r '.OPENTSDB_TSD.Component[0].metrics.default[].metric' >> /etc/ambari-metrics-collector/conf/whitelistedmetrics.txt
@@ -27,19 +26,16 @@ cat OPENTSDB/metrics.json | jq -r '.OPENTSDB_PROXY.Component[0].metrics.default[
 
 # Only perform the remainder on the active head nodes (as defined by headnodehost)
 head_ip=$(getent hosts headnodehost | awk '{ print $1; exit }')
-if [ "$(hostname -i)" == "$head_ip" ] ; then
+is_active_headnode=$(expr "$(hostname -i)" == "$head_ip")
+echo "$(date +%T) This node is active headnode: $is_active_headnode"
 
-    echo "$(date +'%Y-%m-%d %H:%M:%S') Processing service registration on active head node via background script"
-    wget "https://raw.githubusercontent.com/jamesbak/opentsdb-hdi/v0.3/deployment/create-ambari-services.sh" -O /tmp/create-opentsdb-ambari-services.sh
-    chmod 744 /tmp/create-opentsdb-ambari-services.sh
-    mkdir /var/log/opentsdb
-    echo "$(date +'%Y-%m-%d %H:%M:%S') Logging background activity to /var/log/opentsdb/create-ambari-services.out & /var/log/opentsdb/create-ambari-services.err"
-    nohup /tmp/create-opentsdb-ambari-services.sh $user $password $cluster >/var/log/opentsdb/create-ambari-services.out 2>/var/log/opentsdb/create-ambari-services.err &
-else
-
-    echo "$(date +'%Y-%m-%d %H:%M:%S') Node is not currently active head node"
-fi
-echo "$(date +'%Y-%m-%d %H:%M:%S') OpenTSDB has been installed and TSD components have been deployed to all HBase region servers"
+echo "$(date +%T) Processing service registration on active head node via background script"
+wget "https://raw.githubusercontent.com/jamesbak/opentsdb-hdi/v0.3/deployment/create-ambari-services.sh" -O /tmp/create-opentsdb-ambari-services.sh
+chmod 744 /tmp/create-opentsdb-ambari-services.sh
+mkdir /var/log/opentsdb
+echo "$(date +%T) Logging background activity to /var/log/opentsdb/create-ambari-services.out & /var/log/opentsdb/create-ambari-services.err"
+nohup /tmp/create-opentsdb-ambari-services.sh $user $password $cluster $is_active_headnode >/var/log/opentsdb/create-ambari-services.out 2>/var/log/opentsdb/create-ambari-services.err &
+echo "$(date +%T) OpenTSDB has been installed and TSD components have been deployed to all HBase region servers"
 
 
 
